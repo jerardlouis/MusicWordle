d77902048072d20e10bc40b8f6a068ea098951e3
Jerard Guevarra <jerardlguevarra@gmail.com>
2022-01-20 19:41:22 -0500

    Fixed link

--- src/App.tsx
+++ src/App.tsx
@@ -93,7 +93,7 @@ function App() {
       />
       <div className="flex w-80 mx-auto items-center mb-8">
         
-        <img src= "https://www.bowerypresents.com/assets/img/logo.png" />
+        <img src= "https://www.bowerypresents.com/assets/img/logo.png" alt="" />
         <InformationCircleIcon
           className="h-6 w-6 cursor-pointer"
           onClick={() => setIsInfoModalOpen(true)}
